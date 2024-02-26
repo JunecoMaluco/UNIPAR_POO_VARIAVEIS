a752350c47569c3c02d1194925960fa3e2e68b15
JunecoMaluco <161260648+JunecoMaluco@users.noreply.github.com>
2024-02-25 21:46:33 -0300

    AT6.java

---
+++ AT6.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        int numeroProduto = 12; 
+        double precoProduto = 90; 
+        
+        String descricaoProduto;
+        descricaoProduto = "AirPods";
+        
+        System.out.println("Descricao do Produto: " + descricaoProduto);
+        
+        
+        System.out.println("Dados do Produto:");
+        System.out.println("Numero: " + numeroProduto);
+        System.out.println("Preco: $" + precoProduto);
+        System.out.println("Descricao: " + descricaoProduto);
+    }
+}
